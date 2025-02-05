7556e540c6375d21421aa07d0664ca0904dabeb6
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-02-05 13:49:30 +0000

    (wip) add logit method

--- src/margin_calibration.py
+++ src/margin_calibration.py
@@ -22,21 +22,19 @@ class MarginCalibration:
 
     def _ranking_ratio_method(self, w, d):
         return (w/d)*np.log(w/d)-(w/d)+1
-
-    # def _logit_method(self, w, d, lower_bound, upper_bound):
-    #     pass
-        
+     
     def initialize_method(self):
         
         dict_method = {
             "linear":self._linear_method,
-            "ranking_ratio":self._ranking_ratio_method
+            "ranking_ratio":self._ranking_ratio_method,
+            "truncated_linear":self._linear_method
         }
         try:
             return dict_method[self.calibration_method]
 
         except:
-            raise ValueError(f"Invalid value : {self.calibration_method}. Must be one of : 'linear', 'ranking_ratio'")
+            raise ValueError(f"Invalid value : {self.calibration_method}. Must be one of : 'linear', 'ranking_ratio', 'truncated_linear'")
 
     def objective(self, calibration_weights):
         
@@ -50,15 +48,31 @@ class MarginCalibration:
     def constraint(self, calibration_weights):
         return self.calibration_matrix.T@calibration_weights-self.calibration_target
 
-    def calibration(self):
+    def calibration(self,
+                   lower_bound=None,
+                   upper_bound=None):
         
         constraints = {"type":"eq", "fun":self.constraint}
         
         x0 = self.initialize_sampling_weights()
+
+        if self.calibration_method == "truncated_linear":
+            if isinstance(lower_bound, (int, float)) and isinstance(upper_bound, (int, float)):
+                if (lower_bound < 1) and (upper_bound > 1):
+                    sampling_weights = self.initialize_sampling_weights()
+                    bounds = [(lower_bound*d_k,
+                              upper_bound*d_k) for d_k in sampling_weights]
+                else:
+                    raise ValueError("The lower bound should be strictly inferior to 1, the upper bound strictly superior to 1")
+            else:
+                raise TypeError("'lower_bound' and 'upper_bound' must be numeric values")
+        else:
+            bounds=None
         
         return minimize(
             self.objective,
             x0=x0,
             method = "trust-constr",
-            constraints = constraints
+            constraints = constraints,
+            bounds=bounds
         )    
