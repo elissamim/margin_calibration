1ebc3b0f1b139033e97b1275393d70901370bec1
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-02-10 15:39:35 +0000

    (fix) error in vector shape

--- src/margin_calibration.py
+++ src/margin_calibration.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.optimize import minimize
 
 
@@ -13,17 +14,32 @@ class MarginCalibration:
         lower_bound=None,
         upper_bound=None,
         penalty=None,
-        costs=None
+        costs=None,
     ):
 
-        self.sampling_probabilities = sampling_probabilities
-        self.calibration_matrix = calibration_matrix
-        self.calibration_target = calibration_target
+        self.sampling_probabilities = self._to_numpy(sampling_probabilities)
+        self.calibration_matrix = self._to_numpy(calibration_matrix)
+        self.calibration_target = self._to_numpy(calibration_target)
         self.calibration_method = calibration_method
-        self.lower_bound=lower_bound
-        self.upper_bound=upper_bound
-        self.penalty=penalty
-        self.costs=costs
+        self.lower_bound = lower_bound
+        self.upper_bound = upper_bound
+        self.penalty = penalty
+        self.costs = costs
+
+    def _to_numpy(self, data: pd.DataFrame) -> np.ndarray:
+        if isinstance(data, pd.DataFrame):
+            if data.shape[1] == 1:
+                return data.to_numpy().flatten()
+            else:
+                return data.to_numpy()
+        elif isinstance(data, pd.Series):
+            return data.to_numpy()
+        elif isinstance(data, np.ndarray):
+            return data
+        else:
+            raise TypeError(
+                """Input must be a NumPy array or Pandas DataFrame/Series."""
+            )
 
     def initialize_sampling_weights(self):
         return np.array([1 / prob_i for prob_i in self.sampling_probabilities])
@@ -36,11 +52,17 @@ class MarginCalibration:
 
     def _logit_method(self, w, d):
         epsilon = 1e-8
-        x=w/d
-        a = (x-self.lower_bound)*np.log(np.maximum((x-self.lower_bound)/(1-self.lower_bound), epsilon))
-        b = (self.upper_bound-x)*np.log(np.maximum((self.upper_bound-x)/(self.upper_bound-1), epsilon))
-        c = (self.upper_bound-self.lower_bound)/((1-self.lower_bound)*(self.upper_bound-1))
-        return (a+b)/c
+        x = w / d
+        a = (x - self.lower_bound) * np.log(
+            np.maximum((x - self.lower_bound) / (1 - self.lower_bound), epsilon)
+        )
+        b = (self.upper_bound - x) * np.log(
+            np.maximum((self.upper_bound - x) / (self.upper_bound - 1), epsilon)
+        )
+        c = (self.upper_bound - self.lower_bound) / (
+            (1 - self.lower_bound) * (self.upper_bound - 1)
+        )
+        return (a + b) / c
 
     def initialize_method(self):
 
@@ -64,7 +86,7 @@ class MarginCalibration:
         if (self.penalty is None) and (self.costs is None):
 
             sampling_weights = self.initialize_sampling_weights()
-    
+
             return sum(
                 d_k * self.initialize_method()(w_k, d_k)
                 for w_k, d_k in zip(calibration_weights, sampling_weights)
@@ -80,16 +102,18 @@ class MarginCalibration:
                 for w_k, d_k in zip(calibration_weights, sampling_weights)
             )
 
-            margin_gap = self.calibration_matrix.T@calibration_weights-total_target
-            
-            cost = self.penalty*np.dot(margin_gap,np.diag(self.costs)@margin_gap)
+            margin_gap = self.calibration_matrix.T @ calibration_weights - total_target
+
+            cost = self.penalty * np.dot(margin_gap, np.diag(self.costs) @ margin_gap)
 
             return distance + cost
 
         else:
 
-            raise ValueError("""Both 'penalty' and 'costs' must be given, 
-                            in case one is given.""")
+            raise ValueError(
+                """Both 'penalty' and 'costs' must be given, 
+                            in case one is given."""
+            )
 
     def constraint(self, calibration_weights):
         return self.calibration_matrix.T @ calibration_weights - self.calibration_target
@@ -101,9 +125,11 @@ class MarginCalibration:
         elif (self.penalty is not None) and (self.costs is not None):
             constraints = None
         else:
-            raise ValueError("""Both 'penalty' and 'costs' must be given, 
-                            in case one is given.""")
-        
+            raise ValueError(
+                """Both 'penalty' and 'costs' must be given, 
+                            in case one is given."""
+            )
+
         x0 = self.initialize_sampling_weights()
 
         if self.calibration_method in ["truncated_linear", "logit"]:
