e2017fb780cf07b1a6aa0620dd4c847434634ff3
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-02-11 15:00:28 +0000

    (refacto) tests

--- tests/test_margin_calibration.py
+++ tests/test_margin_calibration.py
@@ -4,59 +4,30 @@ import sys
 sys.path.append("../src")
 from margin_calibration import MarginCalibration
 
-sampling_probabilities = np.random.uniform(low=0, high=1, size=(10, 1))
-calibration_matrix = np.random.uniform(low=0, high=100, size=(10, 2))
-calibration_target = calibration_matrix.sum(axis=0)*100
-calibration_target = calibration_target.reshape(-1, 1)
 
 def test_linear_distance():
     
-    mc=MarginCalibration(sampling_probabilities,
-                        calibration_matrix,
-                        calibration_target,
-                        calibration_method = "linear")
+    mc=MarginCalibration()
     
     assert mc._linear_method(1, 1) == 0
 
 def test_truncated_linear_distance():
 
-    mc=MarginCalibration(sampling_probabilities,
-                        calibration_matrix,
-                        calibration_target,
-                        calibration_method = "truncated_linear",
-                        lower_bound = 0.5,
-                        upper_bound = 1.5)
+    mc=MarginCalibration()
     
     assert mc._linear_method(1, 1) == 0
 
 def test_logit_distance():
 
-    mc=MarginCalibration(sampling_probabilities,
-                        calibration_matrix,
-                        calibration_target,
-                        calibration_method = "logit",
-                        lower_bound=0.5,
-                        upper_bound=1.5)
+    mc=MarginCalibration()
 
     assert mc._logit_method(1, 1) == 0
 
 def test_raking_ratio_distance():
 
-    mc=MarginCalibration(sampling_probabilities,
-                        calibration_matrix,
-                        calibration_target,
-                        calibration_method = "raking_ratio")
+    mc=MarginCalibration()
     
     assert mc._raking_ratio_method(1, 1) == 0
-
-def test_ndarray_creator():
-
-    mc=MarginCalibration(sampling_probabilities,
-                        calibration_matrix,
-                        calibration_target)
-
-    assert all(isinstance(x, np.ndarray)
-              for x in [mc.sampling_probabilities, mc.calibration_matrix, mc.calibration_target])
     
 if __name__ == "__main__":
     pytest.main()
