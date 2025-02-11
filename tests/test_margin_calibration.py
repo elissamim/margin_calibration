b931494caea1ffe34005e63894b40e106b58706a
elissamim <mouad.el-issami@igf.finances.gouv.fr>
2025-02-11 15:02:19 +0000

    (refacto) tests

--- tests/test_margin_calibration.py
+++ tests/test_margin_calibration.py
@@ -4,22 +4,15 @@ import sys
 sys.path.append("../src")
 from margin_calibration import MarginCalibration
 
-
 def test_linear_distance():
     
     mc=MarginCalibration()
     
     assert mc._linear_method(1, 1) == 0
 
-def test_truncated_linear_distance():
-
-    mc=MarginCalibration()
-    
-    assert mc._linear_method(1, 1) == 0
-
 def test_logit_distance():
 
-    mc=MarginCalibration()
+    mc=MarginCalibration(lower_bound=.9, upper_bound=1.1)
 
     assert mc._logit_method(1, 1) == 0
 
